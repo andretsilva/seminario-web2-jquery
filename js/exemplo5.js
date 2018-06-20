d36cca76e7705f649414b9edfa7c6b23354b0b21
Victor <victorpassis@gmail.com>
2018-06-20 15:04:21 -0300

    Add plugins

---
+++ js/exemplo5.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function(){
+	$( "#accordion" ).accordion();
+
+	var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableTags
+    });
+
+    $(".center").slick({
+	    dots: true,
+	    infinite: true,
+	    centerMode: true,
+	    slidesToShow: 5,
+	    slidesToScroll: 3
+	});
+});
